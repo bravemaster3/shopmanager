7872e12fed06a8118920015a553a10a1fc9cff48
bravemaster <noumonvikoffidodji@hotmail.fr>
2022-11-11 20:19:08 +0100

    Update settings.py

--- shopmanager/settings.py
+++ shopmanager/settings.py
@@ -98,7 +98,7 @@ REST_FRAMEWORK = {
   #'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
       'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
 
