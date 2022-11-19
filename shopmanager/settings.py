96fc457cd0ec251e3a88dccacbb64dbf17d021e4
bravemaster <noumonvikoffidodji@hotmail.fr>
2022-11-19 20:08:51 +0100

    Update settings.py

--- shopmanager/settings.py
+++ shopmanager/settings.py
@@ -167,7 +167,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE="whitenoise.storage.CompressedStaticFilesStorage"#"whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
